d4e29e3f4714a541b2f996e1b0241dc7200332bf
codename <daction-z@hotmail.com>
2015-12-20 04:07:30 -0800

    Sync

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('<H1>Hello</H1> express');
+} );
+
+app.get('/me', function(req, res) {
+    res.send('@planetoftheweb');
+} );
+
+app.get('/who/:name?', function(req, res) {
+    var name = req.params.name;
+    res.send(name + ' was here');
+} );
+
+// Example test URL: http://localhost:3000/who/david/theboss/
+app.get('/who/:name?/:title?', function(req, res) {
+    var name = req.params.name;
+    var title = req.params.title;
+    res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
+} );
+
+// Irrelevant web pages
+app.get('*', function(req, res) {
+    res.send('Bad Route');
+} );
+
+// Server listen port
+var server = app.listen(3000, function() {
+   console.log('Listening on port 3000');
+});
+
+
